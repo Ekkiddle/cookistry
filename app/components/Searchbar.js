70c562a67fd258a93d3694a30fed8ed9ddc0d9f5
Ekkiddle <ekkiddle@gmail.com>
2024-10-30 14:47:23 -0600

    Completed Nav and Search bar

--- app/components/Searchbar.js
+++ app/components/Searchbar.js
@@ -1,11 +1,55 @@
 "use client"
 
 // Search bar component for Navbar
-import { useState, ChangeEvent } from "react";
+import { useRef, useState, useEffect } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { CiFilter } from "react-icons/ci";
+import { Button, FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography} from "@mui/material";
 
 const Searchbar = () => {
+    const componentRef = useRef(null);
+
+    useEffect(() => {
+        // Function to check if the click is outside
+        function handleClickOutside(event) {
+          if (componentRef.current && !componentRef.current.contains(event.target)) {
+            setOpen(false);
+          }
+        }
+    
+        // Bind the event listener to the document
+        document.addEventListener('mousedown', handleClickOutside);
+    
+        // Clean up the event listener on component unmount
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const [levelFilter, setLevelFilter] = useState({
+        beginner: true,
+        intermediate: true,
+        advanced: true,
+      });
+      const [typeFilter, setTypeFilter] = useState({
+        appetizer: true,
+        entree: true,
+        dessert: true,
+      });
+    
+      // Update filters when checkboxes change
+      const handleLevelChange = (event) => {
+        setLevelFilter({ ...levelFilter, [event.target.name]: event.target.checked });
+      };
+    
+      const handleTypeChange = (event) => {
+        setTypeFilter({ ...typeFilter, [event.target.name]: event.target.checked });
+      };
+
     const [inputValue, setValue] = useState("")
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {setOpen(true)};
+    const handleClose = () => {setOpen(false)};
 
     const handleChange = (event) => {
         setValue(event.target.value);
@@ -21,9 +65,19 @@ const Searchbar = () => {
         }
     }
 
+    const filter = () => {
+        const activeLevels = Object.keys(levelFilter).filter((level) => levelFilter[level]);
+        const activeTypes = Object.keys(typeFilter).filter((type) => typeFilter[type]);
+    
+        console.log('Active Levels:', activeLevels);
+        console.log('Active Types:', activeTypes);
+        handleClose();
+    };
+
+
     return(
-        <div className="border-[2px] border-solid border-slate-500 flex flex-row items-center p-1 gap-3 rounded-[15px] w-full">
-            <IoIosSearch className="text-lg md:text-xl font-bold"/>
+        <div className="border-[2px] border-solid border-slate-500 flex flex-row items-center gap-3 rounded-[15px] w-full h-6">
+            <IoIosSearch className="text-lg md:text-xl font-bold m-1"/>
 
             <input type="text"
                 id="inputId"
@@ -31,7 +85,80 @@ const Searchbar = () => {
                 value={inputValue || ""} onChange={handleChange}
                 onKeyDown={handleKeyPress}
                 className="bg-[transparent] outline-none border-none w-full text-xs md:text-sm" />
-
+            <div className="relative h-full">
+                <button className="bg-slate-500 h-full rounded-r-[15px] w-10 flex items-center justify-center hover:bg-slate-700 text-white hover:text-slate-500"
+                    onClick={handleOpen}>
+                    <CiFilter />
+                    
+                </button>
+                {open ? (
+                    <div ref={componentRef} className="absolute flex flex-col p-2 bg-colour4 top-5 right-0 shadow-xl rounded-sm text-colour2 gap-2">
+                        <Typography style={{ textDecoration: 'underline' }}>
+                            Filter Settings
+                        </Typography>
+                        {/* Level Filter */}
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Level</FormLabel>
+                            <FormGroup >
+                            {['beginner', 'intermediate', 'advanced'].map((level) => (
+                                <FormControlLabel
+                                key={level}
+                                control={
+                                    <Checkbox
+                                    checked={levelFilter[level]}
+                                    onChange={handleLevelChange}
+                                    name={level}
+                                    sx={{
+                                        color: "#14213d", // unchecked color
+                                        '&.Mui-checked': {
+                                          color: "#14213d", // checked color
+                                        },
+                                        '& .MuiSvgIcon-root': {
+                                          fontSize: 16, // change size of the icon
+                                        }
+                                    }}
+                                    />
+                                }
+                                label={level.charAt(0).toUpperCase() + level.slice(1)}
+                                />
+                            ))}
+                            </FormGroup>
+                        </FormControl>
+                        {/* Type Filter */}
+                        <FormControl component="fieldset" sx={{ mt: 2 }}>
+                            <FormLabel component="legend">Type</FormLabel>
+                            <FormGroup>
+                            {['appetizer', 'entree', 'dessert'].map((type) => (
+                                <FormControlLabel
+                                key={type}
+                                control={
+                                    <Checkbox
+                                    checked={typeFilter[type]}
+                                    onChange={handleTypeChange}
+                                    name={type}
+                                    sx={{
+                                        color: "#14213d", // unchecked color
+                                        '&.Mui-checked': {
+                                          color: "#14213d", // checked color
+                                        },
+                                        '& .MuiSvgIcon-root': {
+                                          fontSize: 16, // change size of the icon
+                                        }
+                                    }}
+                                    />
+                                }
+                                label={type.charAt(0).toUpperCase() + type.slice(1)}
+                                />
+                            ))}
+                            </FormGroup>
+                        </FormControl>
+                        <div className="flex flex-row gap-1 w-full h-5 mt-2">
+                            <Button variant="contained" color="success" onClick={filter}>Confirm</Button>
+                            <Button variant="contained" color="primary" onClick={handleClose}>Cancel</Button>
+                        </div>
+                    </div>) :(<div></div>)
+                }
+            </div>
         </div>
     );
 }
--- app/components/navbar.js
+++ app/components/navbar.js
@@ -7,16 +7,18 @@ const NavBar = () => {
     <header id="header" className="w-screen items-center bg-transparent w-full fixed">
       <nav className="flex flex-col md:flex-row gap-1 md:gap-3 items-center justify-between stretch w-screen h-full p-5 py-2 bg-colour2">
         <Link href={"/"}>
-            <h1 className="text-colour4">Cookistry</h1>
+            <h1 className="text-colour4 font-serif font-bold text-lg">Cookistry</h1>
         </Link>
-        <Searchbar />
-        <div className="flex flex-row gap-3 text-xs md:text-sm mt-auto">
-          <Link href={"/"}>
-              <h1 className="text-colour4 hover:text-colour3">Recipes</h1>
-          </Link>
-          <Link href={"/techniques"}>
-              <h1 className="text-colour4 hover:text-colour3">Resources</h1>
-          </Link>
+        <div className="flex flex-col md:flex-row w-full gap-1 md:gap-3 max-w-[700px]">
+          <Searchbar />
+          <div className="flex flex-row gap-3 text-xs md:text-sm mt-auto justify-evenly w-full md:w-auto mt-3">
+            <Link href={"/"}>
+                <u className="text-colour4 hover:text-colour3">Recipes</u>
+            </Link>
+            <Link href={"/techniques"}>
+                <u className="text-colour4 hover:text-colour3">Resources</u>
+            </Link>
+          </div>
         </div>
       </nav>
       <div className="h-2 w-full bg-colour3"></div>
