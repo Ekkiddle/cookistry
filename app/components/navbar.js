a57f7a930a78b975a93d5637ac8972843a500691
Ekkiddle <ekkiddle@gmail.com>
2024-10-31 14:07:02 -0600

    minimal updates to navBar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -4,7 +4,7 @@ import Searchbar from "./Searchbar";
 
 const NavBar = () => {
     return(
-    <header id="header" className="w-screen items-center bg-transparent w-full fixed">
+    <header id="header" className="w-screen items-center bg-transparent w-full fixed z-20">
       <nav className="flex flex-col md:flex-row gap-1 md:gap-3 items-center justify-between stretch w-screen h-full p-5 py-2 bg-colour2">
         <Link href={"/"}>
             <h1 className="text-colour4 font-serif font-bold text-lg">Cookistry</h1>
